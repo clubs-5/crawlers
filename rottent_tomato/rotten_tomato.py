c72167bb0d5682cb0700b30edcb716c28c4f1c64
veritaspoutine <14975224+veritaspoutine@users.noreply.github.com>
2020-10-31 15:22:02 +0800

    defined main() and get_seasons_reviews_page_html

--- rottent_tomato/rotten_tomato.py
+++ rottent_tomato/rotten_tomato.py
@@ -9,48 +9,62 @@ header_agent = {
 }
 
 keyword = input("give me a show name:")
-url = 'https://www.rottentomatoes.com/napi/search/all?type=tv&searchQuery={}'.format(keyword)
+url_tomato = 'https://www.rottentomatoes.com/napi/search/all?type=tv&searchQuery={}'.format(keyword)
 session = HTMLSession()
 
 
+def main():
+    response_code =  establish_session(url_tomato)
+    result_html_text = fetch_shows_search_page_html(response_code)
+    show_main_page_link = get_show_main_page_link(result_html_text)
+    reviews_page_link_by_season = show_main_page_link + '/s1/reviews'
+    review_page_html = get_seasons_reviews_page_html(reviews_page_link_by_season)
 
+    
+    print(review_page_html)
+
+
+
+
+
+def establish_session(url):
+    response = session.get(url, headers = header_agent)
+
+    return response
 
 
 # get show's search result html
-def fetch():
+def fetch_shows_search_page_html(res):
+    code = res.status_code
+    if code == 200:
+        html = res.html.find('*', first = True)
+        html_text = html.text
+        return html_text
+
+    else:
+        return 'Non 200'
+
+def get_show_main_page_link(s):
+    urls = re.findall(r"https://rottentomatoes.com/tv/\w+", s)
+
+    return urls[0]
+
+def get_seasons_reviews_page_html(url):
     
-    req = session.get(url, headers = header_agent)
-    html = req.html.find('*', first = True)
-    html_text = html.text
-    
-    return html_text
-
-
-def get_show_page_links(html_text):
-    urls = re.findall(r"https://rottentomatoes.com/tv/\w+",html_text)
-
-    return url[0]
-
-def get_seasons_page_html(show_link):
-    response = 
+    html = establish_session(url).html.find('.critic__review-quote' )
+    for i in html:
+        print(i.text)
 
 #def get_season_rating():
 
 #def get_season_reviews():
+'''
+response =  establish_session(url_tomato)
+result_html_text = fetch_shows_search_page_html(response)
+show_main_page_link = get_show_main_page_link(result_html_text)
+    
+print(show_main_page_link)
+'''
 
-
-html = fetch()
-show_link=get_show_page_links(html)
-
-
-print('==============')
-#print(html)
-#print('==============')
-print(link[0])
-
-
-#def main():
-
-
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
