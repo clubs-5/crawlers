870a217161498d1ed533876a94cf458c246bee9c
brucechen <alex0228@yahoo.com>
2020-11-13 10:50:04 +0800

    update5

--- omdb_data/omdb_data_1-399999.py
+++ omdb_data/omdb_data_1-399999.py
@@ -6,7 +6,7 @@ import pandas as pd
 file = input('give me a csv file name only: ')
 df  = pd.read_csv('./{}.csv'.format(file))
 
-with open("omdb_all.json1",'w+',encoding='utf-8') as f1:
+with open("omdb_all.json2",'w+',encoding='utf-8') as f1:
     try:
         n = 0
         for i in df['name']:
